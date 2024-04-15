d6fa9781b7001333d49fcb444172f0186403f30c
Dvzeira <135902111+Dvzeira@users.noreply.github.com>
2024-04-15 19:28:49 -0300

    Update contato.js

--- sprint 4/scripts/contato.js
+++ sprint 4/scripts/contato.js
@@ -13,96 +13,96 @@ let telefoneInput = document.getElementById("telefone");
 let telefoneCampo = document.querySelector('label[for="telefone"]');
 let telAjuda = document.getElementById("tel-ajuda")
 
-if(nomeInput){
-nomeInput.addEventListener("input", () => {
+if (nomeInput) {
+  nomeInput.addEventListener("input", () => {
     const usuario = nomeInput.value;
     if (usuario.length >= 3) {
-        nomeInput.classList.remove("error");
-        nomeInput.classList.add("correct");
-        nomeAjuda.style.display = "none";
+      nomeInput.classList.remove("error");
+      nomeInput.classList.add("correct");
+      nomeAjuda.style.display = "none";
 
-      } else if(usuario.length === 0) {
-        nomeInput.classList.remove("correct");
-        nomeInput.classList.remove("error");
-        nomeAjuda.style.display = "none";
-    } else{
-        nomeInput.classList.remove("correct");
-        nomeInput.classList.add("error");
-        nomeAjuda.style.display = "block";
-      }
+    } else if (usuario.length === 0) {
+      nomeInput.classList.remove("correct");
+      nomeInput.classList.remove("error");
+      nomeAjuda.style.display = "none";
+    } else {
+      nomeInput.classList.remove("correct");
+      nomeInput.classList.add("error");
+      nomeAjuda.style.display = "block";
+    }
   });
 }
 
-  emailInput.addEventListener("input", () => {
-    const usuarioEmail = emailInput.value;
-    if (usuarioEmail.includes('@')) {
-        emailInput.classList.remove("error");
-        emailInput.classList.add("correct");
-        emailAjuda.style.display = "none";
+emailInput.addEventListener("input", () => {
+  const usuarioEmail = emailInput.value;
+  if (usuarioEmail.includes('@')) {
+    emailInput.classList.remove("error");
+    emailInput.classList.add("correct");
+    emailAjuda.style.display = "none";
 
-      } else if(usuarioEmail.length === 0) {
-        emailInput.classList.remove("correct");
-        emailInput.classList.remove("error");
-        emailAjuda.style.display = "none";
+  } else if (usuarioEmail.length === 0) {
+    emailInput.classList.remove("correct");
+    emailInput.classList.remove("error");
+    emailAjuda.style.display = "none";
 
-    } else{
-        emailInput.classList.remove("correct"); 
-        emailInput.classList.add("error");
-        emailAjuda.style.display = "block";
-      }
-  });
+  } else {
+    emailInput.classList.remove("correct");
+    emailInput.classList.add("error");
+    emailAjuda.style.display = "block";
+  }
+});
 
 
-  telefoneInput.addEventListener("input", () => {
-    const usuarioTel = telefoneInput.value;
-    if (/^\d+$/.test(usuarioTel) && (usuarioTel.length ===11)) { 
-        telefoneInput.classList.remove("error");
-        telefoneInput.classList.add("correct");
-        telAjuda.style.display = "none";
+telefoneInput.addEventListener("input", () => {
+  const usuarioTel = telefoneInput.value;
+  if (/^\d+$/.test(usuarioTel) && (usuarioTel.length === 11)) {
+    telefoneInput.classList.remove("error");
+    telefoneInput.classList.add("correct");
+    telAjuda.style.display = "none";
 
-      } else if(usuarioTel.length === 0) {
-        telefoneInput.classList.remove("correct");
-        telefoneInput.classList.remove("error");
-        telAjuda.style.display = "none";
+  } else if (usuarioTel.length === 0) {
+    telefoneInput.classList.remove("correct");
+    telefoneInput.classList.remove("error");
+    telAjuda.style.display = "none";
 
-    } else{
-        telefoneInput.classList.remove("correct"); 
-        telefoneInput.classList.add("error");
-        telAjuda.style.display = "block";
+  } else {
+    telefoneInput.classList.remove("correct");
+    telefoneInput.classList.add("error");
+    telAjuda.style.display = "block";
 
-      }
-  });
+  }
+});
 
 
 // Mostrar popup de campo obrigatório no campo input
-if(nomeInput){
-nomeInput.addEventListener('focus', function(){
+if (nomeInput) {
+  nomeInput.addEventListener('focus', function () {
     nomeCampo.classList.add('required-popup')
-})
+  })
 }
 
-emailInput.addEventListener('focus', function(){
-    emailCampo.classList.add('required-popup')
+emailInput.addEventListener('focus', function () {
+  emailCampo.classList.add('required-popup')
 })
 
-telefoneInput.addEventListener('focus', function(){
-    telefoneCampo.classList.add('required-popup')
+telefoneInput.addEventListener('focus', function () {
+  telefoneCampo.classList.add('required-popup')
 })
 
 
 // Ocultar popup de campo obrigatório
-if(nomeInput){
-nomeInput.addEventListener('blur', function(){
+if (nomeInput) {
+  nomeInput.addEventListener('blur', function () {
     nomeCampo.classList.remove('required-popup')
-})
+  })
 }
 
-emailInput.addEventListener('blur', function(){
-    emailCampo.classList.remove('required-popup')
+emailInput.addEventListener('blur', function () {
+  emailCampo.classList.remove('required-popup')
 })
 
-telefoneInput.addEventListener('blur', function(){
-    telefoneCampo.classList.remove('required-popup')
+telefoneInput.addEventListener('blur', function () {
+  telefoneCampo.classList.remove('required-popup')
 })
 
 
@@ -112,11 +112,9 @@ telefoneInput.addEventListener('blur', function(){
 
 /////código envio do card - Davi /////
 const contacts = document.querySelector('.contacts');
-console.log(contacts);
+const form = document.querySelector('form');
 
-const btnEnviar = document.getElementById('btnEnviar');
-
-btnEnviar.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault(); // Evita o envio do formulário e a recarga da página
 
   const nome = document.getElementById('nome').value;
@@ -125,11 +123,32 @@ btnEnviar.addEventListener('click', (event) => {
   const telefone = document.getElementById('telefone').value;
   const mensagem = document.getElementById('mensagem').value;
 
-  // Cria um novo elemento de contato
+
+  if (nome === '' || email === '' || mensagem === '') {
+    alert('Por favor, preencha todos os campos obrigatórios.');
+    return;
+  }
+  if (nome.length < 3) {
+    alert('Por favor, insira um nome com pelo menos 3 caracteres.');
+    return;
+  }
+
+  if (!/^\d{11}$/.test(telefone)) {
+    alert('Por favor, insira um telefone válido com 11 dígitos.');
+    return;
+  }
+
+  // Validação do email: verifica se o formato está correto
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert('Por favor, insira um email válido.');
+    return;
+  }
+
   const contactDiv = document.createElement('div');
   contactDiv.classList.add('contact');
 
-  // Preenche o elemento de contato com as informações do formulário
+
   contactDiv.innerHTML = `
         <h3>Informações de Contato:</h3>
         <p>Nome: ${nome} ${sobreNome}</p>
@@ -141,19 +160,6 @@ btnEnviar.addEventListener('click', (event) => {
   // Adiciona o elemento de contato à seção de contatos
   contacts.appendChild(contactDiv);
 
-  //Limpa os campos de entrada após o clique
-  const form = document.forms[0];
+  // Limpa os campos de entrada após o envio do formulário
   form.reset();
 });
-
-
-const btnConcluir = document.querySelector('.btn-concluir');
-
-btnConcluir.addEventListener('click', function(event) {
-    const camposError = document.querySelectorAll('.error');
-    if (camposError.length > 0) {
-        event.preventDefault();
-        alert('Por favor, preencha os campos corretamente para prosseguir!');
-    }
-});
-
