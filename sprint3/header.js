05998bd4ce5695bd772c0a65b18cb96218b0408f
jhonatans <j.rennan@outlook.com>
2024-04-28 20:25:25 -0300

    Ajustes para deploy

--- sprint3/header.js
+++
@@ -1,7 +0,0 @@
-const hamburguerMenu = document.querySelector('.hamburguer-menu')
-const menu = document.querySelector('.headerBtnGroup');
-
-
-hamburguerMenu.addEventListener('click', function () {
-  menu.classList.toggle('show');
-});
